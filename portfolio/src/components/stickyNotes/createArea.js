3f179ce8c29e90548f207d26a7fd4e9dc4c23b03
SushiAbuse <justine.tran@mavs.uta.edu>
2024-02-07 18:51:45 -0600

    createArea - grant access and note

--- portfolio/src/components/stickyNotes/createArea.js
+++ portfolio/src/components/stickyNotes/createArea.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './createArea.css';
 
 import noteBook from './img/note.png';
 
-const createArea = ({ onClick }) => {
+const CreateArea = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleClick = () => {
+    const username = prompt("Please enter your username:");
+    const password = prompt("Please enter your password:");
+    if (username === "admin" && password === "makeanote") {
+      // Logic to add a note or navigate to note adding area
+      alert("Access granted. You can now add a note.");
+    } else {
+      alert("Access denied. Incorrect username or password.");
+    }
+  };
+
   return (
-    <div className="icon-container" onClick={onClick}>
+    <div 
+      className="icon-container" 
+      onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && <div className="tooltip">Add a note</div>}
       <div className="notebook-icon">
-        <img src={noteBook} alt="NoteBookIcon"/>
+        <img src={noteBook} alt="NoteBook Icon"/>
       </div>
     </div>
   );
 };
 
-export default createArea;
+export default CreateArea;
