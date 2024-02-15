b66587c80c9cf9ad2b5b3116f0bd738bf1901ccb
SushiAbuse <justine.tran@mavs.uta.edu>
2024-02-14 18:12:25 -0600

    Chimpmunk and Nut Showing Up

--- portfolio/src/components/autoProjectTimeLine/autoProjectTimeLine.js
+++ portfolio/src/components/autoProjectTimeLine/autoProjectTimeLine.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './autoProjectTimeline.css';
 import timeline from './img/timeline.png';
+import chipmunk from './img/chipmunk.png';
+import nut from './img/nut.png';
 
 function AutoProjectTimeLine() {
   const [showForm, setShowForm] = useState(false);
@@ -20,9 +22,10 @@ function AutoProjectTimeLine() {
 
   const submitNote = (event) => {
     event.preventDefault();
-    setProjects([...projects, project]);
-    setProject({ title: '', content: '', URL: '' }); // Reset form
-    setShowForm(false); // Hide form
+    const newProject = {...project, visible: Math.random() < 0.5};
+    setProjects([...projects, newProject]);
+    setProject({ title: '', content: '', URL: '' });
+    setShowForm(false);
   };
 
   return (
@@ -48,6 +51,11 @@ function AutoProjectTimeLine() {
         <div key={index} className="project-timeline">
           <div className="timeline-vertical"></div>
           <div className={index % 2 === 0 ? "timeline-horizontal-left" : "timeline-horizontal-right"}>
+          {index % 2 === 0 ? (
+            <img src={chipmunk} alt="Chipmunk" style={{ position: 'absolute', right: '50%', top: '50%', transform: 'translateY(-50%) translateY(-10px)', width: '40px', height: '40px', visibility: project.visible ? 'visible' : 'hidden' }} />
+          ) : (
+            <img src={nut} alt="Nut" style={{ position: 'absolute', left: '50%', top: '-20%', transform: 'translateY(-50%) translateY(-10px)', width: '20px', height: '20px', visibility: project.visible ? 'visible' : 'hidden' }} />
+          )}
             <span>{project.title}: {project.content} - {project.URL}</span>
           </div>
         </div>
