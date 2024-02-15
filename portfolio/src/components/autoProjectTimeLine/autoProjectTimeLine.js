79b2bb3739758f4beb107f946e343ad8f83449db
SushiAbuse <justine.tran@mavs.uta.edu>
2024-02-14 18:36:33 -0600

    Project Timeline Date (Finished)

--- portfolio/src/components/autoProjectTimeLine/autoProjectTimeLine.js
+++ portfolio/src/components/autoProjectTimeLine/autoProjectTimeLine.js
@@ -7,7 +7,7 @@ import nut from './img/nut.png';
 function AutoProjectTimeLine() {
   const [showForm, setShowForm] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
-  const [project, setProject] = useState({ title: '', content: '', URL: '' });
+  const [project, setProject] = useState({ title: '', content: '', URL: '', date: '' });
   const [projects, setProjects] = useState([]);
 
   const toggleForm = () => setShowForm(!showForm);
@@ -24,7 +24,7 @@ function AutoProjectTimeLine() {
     event.preventDefault();
     const newProject = {...project, visible: Math.random() < 0.5};
     setProjects([...projects, newProject]);
-    setProject({ title: '', content: '', URL: '' });
+    setProject({ title: '', content: '', URL: '', date: '' });
     setShowForm(false);
   };
 
@@ -44,6 +44,7 @@ function AutoProjectTimeLine() {
           <div className="form-container">
             <form onSubmit={submitNote}>
               <input name="title" onChange={handleChange} value={project.title} placeholder="Project Title" />
+              <input name="date" type="date" onChange={handleChange} value={project.date} placeholder="Project Date"></input>
               <textarea name="content" onChange={handleChange} value={project.content} placeholder="Describe Your Project Here (max 500 characters)" rows="3" maxLength="500"/>
               <textarea name="URL" onChange={handleChange} value={project.URL} placeholder="Paste Project URL Here" rows="1" />
               <button type="submit">Add Project</button>
@@ -56,9 +57,15 @@ function AutoProjectTimeLine() {
           <div className="timeline-vertical"></div>
           <div className={index % 2 === 0 ? "timeline-horizontal-left" : "timeline-horizontal-right"}>
           {index % 2 === 0 ? (
-            <img src={chipmunk} alt="Chipmunk" style={{ position: 'absolute', right: '50%', top: '50%', transform: 'translateY(-50%) translateY(-10px)', width: '40px', height: '40px', visibility: project.visible ? 'visible' : 'hidden' }} />
+            <>
+              <span className="project-date-left">{project.date}</span>
+              <img src={chipmunk} alt="Chipmunk" style={{ position: 'absolute', right: '50%', top: '50%', transform: 'translateY(-50%) translateY(-10px)', width: '40px', height: '40px', visibility: project.visible ? 'visible' : 'hidden' }} />
+            </>
           ) : (
-            <img src={nut} alt="Nut" style={{ position: 'absolute', left: '50%', top: '-20%', transform: 'translateY(-50%) translateY(-10px)', width: '20px', height: '20px', visibility: project.visible ? 'visible' : 'hidden' }} />
+            <>
+              <img src={nut} alt="Nut" style={{ position: 'absolute', left: '50%', top: '-20%', transform: 'translateY(-50%) translateY(-10px)', width: '20px', height: '20px', visibility: project.visible ? 'visible' : 'hidden' }} />
+              <span className="project-date-right">{project.date}</span>
+            </>
           )}
             <span>{project.title}: {project.content} - {project.URL}</span>
           </div>
